5f298a3e3c24a665d369bdedb7088893d016fad5
Humberto Filho <hleitefilho@hotmail.com>
2022-07-11 12:04:06 -0300

    First version for this pagination library

---
+++ src/Berger.Extensions.Pagination.Tests/Paginations/PaginationTest.cs
@@ -0,0 +1,62 @@
+using berger.extensions.pagination.Services;
+using System.Collections.Generic;
+using NUnit.Framework;
+using System.Linq;
+
+namespace berger.extensions.pagination.tests
+{
+    public class PaginationTest
+    {
+        private const int TotalCount = 100;
+        private const int Pages = 5;
+        private const int Limit = TotalCount / Pages;
+        private IList<string> StringList = new string[TotalCount].ToList();
+
+        [Test]
+        [Order(0)]
+        public void ShouldPaginate()
+        {
+            var pagination = StringList.Paginate(1, Limit);
+
+            Assert.IsTrue(pagination.Pages == Pages);
+            Assert.IsTrue(pagination.Items.Count == Limit);
+            Assert.IsTrue(pagination.TotalCount == TotalCount);
+            Assert.IsTrue(pagination.Limit == Limit);
+        }
+
+        [Test]
+        [Order(1)]
+        public void ShouldHaveNextPage()
+        {
+            var pagination = StringList.Paginate(1, Limit);
+
+            Assert.IsTrue(pagination.PageInfo.HasNextPage);
+        }
+
+        [Test]
+        [Order(2)]
+        public void ShouldNotHaveNextPage()
+        {
+            var pagination = StringList.Paginate(5, Limit);
+
+            Assert.IsTrue(!pagination.PageInfo.HasNextPage);
+        }
+
+        [Test]
+        [Order(3)]
+        public void ShouldHavePreviousPage()
+        {
+            var pagination = StringList.Paginate(2, Limit);
+
+            Assert.IsTrue(pagination.PageInfo.HasPreviousPage);
+        }
+        [Test]
+        [Order(4)]
+        public void ShouldNotHavePreviousPage()
+        {
+            var pagination = StringList.Paginate(1, Limit);
+
+            Assert.IsTrue(!pagination.PageInfo.HasPreviousPage);
+        }
+    }
+}
---
+++ src/Berger.Extensions.Pagination/Services/PaginationExtension.cs
@@ -0,0 +1,47 @@
+﻿using berger.extensions.pagination.Models;
+
+namespace berger.extensions.pagination.Services
+{
+    public static class PaginationExtension
+    {
+        public static Pagination<T> Paginate<T>(this IQueryable<T> query, int page, int limit) where T : class
+        {
+            return query.MakePagination(page, limit);
+        }
+
+        public static Pagination<T> Paginate<T>(this IList<T> list, int page, int limit) where T : class
+        {
+            return list.AsQueryable().MakePagination(page, limit);
+        }
+
+        private static Pagination<T> MakePagination<T>(this IQueryable<T> query, int page, int limit) where T : class
+        {
+            var pagination = new Pagination<T>();
+
+            var records = query.Count();
+            var pages = (double)records / limit;
+            var round = (int)Math.Ceiling(pages);
+            var skip = (page - 1) * limit;
+
+            var results = query.Count() <= limit ? query : query.Skip(skip).Take(limit);
+
+            var pageInfo = new PageInfo()
+            {
+                Current = page,
+                Previous = (page - 1 == 0) ? 1 : page - 1,
+                Next = (page + 1 == round + 1) ? round : page + 1,
+                HasNextPage = round >= page + 1,
+                HasPreviousPage = page > 1
+            };
+
+            pagination.TotalCount = records;
+            pagination.Limit = limit;
+            pagination.Pages = round;
+            pagination.Items = results.ToList();
+
+            pagination.PageInfo = pageInfo;
+
+            return pagination;
+        }
+    }
+}
