785d24de179b18bbce389fc9175adaf6d5897de3
Rafael Cronemberger <rafael@berger.com.br>
2023-08-13 02:17:23 -0300

    New operations

---
+++ src/Berger.Extensions.Pagination/Constants/PaginationConstant.cs
@@ -0,0 +1,8 @@
+﻿namespace Berger.Extensions.Pagination
+{
+    public static class Directive
+    {
+        public const string Take = "take";
+        public const string Skip = "skip";
+    }
+}
